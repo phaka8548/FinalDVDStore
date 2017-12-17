ec51ab5cab391bfd00b0f558213b7dbe0bb3c856
phaka8548 <34377705+phaka8548@users.noreply.github.com>
2017-12-16 20:48:36 -0600

    Update Person.java
    Added javadocs

--- DVD Store/src/Person.java
+++ DVD Store/src/Person.java
@@ -5,7 +5,10 @@ import java.io.Serializable;
 public class Person implements Serializable{
 
 	String fullName;
-
+	/**
+	 * set the fullname of the person object
+	 * @param fullName
+	 */
 	public Person(String fullName) {
 		super();
 		this.fullName = fullName;
